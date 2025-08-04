48bb8c26483711c9ade435b6fb4cd94f03c4a09d
kraglr <klaguilar.1doc@gmail.com>
2025-08-04 16:25:44 +0800

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,5 +1,34 @@
 // tailwind.config.js
-module.exports = {
+import flowbitePlugin from "flowbite/plugin";
+export default {
     darkMode: 'class', // or 'media' if you prefer system preference
-    // other config...
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    ],
+
+    plugins: [flowbitePlugin],
+    theme: {
+        extend: {
+            animation: {
+                'bounce-in': 'bounceIn 0.6s ease-out',
+            },
+            keyframes: {
+                bounceIn: {
+                    '0%': {
+                        transform: 'scale(0.9)',
+                        opacity: '0',
+                    },
+                    '60%': {
+                        transform: 'scale(1.05)',
+                        opacity: '1',
+                    },
+                    '100%': {
+                        transform: 'scale(1)',
+                    },
+                },
+            },
+        },
+    },
 };
